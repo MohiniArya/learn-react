b1a07dc67bfa4b7e2feb6a016662dc1a4b60384b
MohiniArya <amohiniarya59@gmail.com>
2024-02-14 20:09:56 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css'; 
+import ClassComonents from './Components/ClassComonents';
+
+function App() {
+  return (
+    <div className="App">
+   <ClassComonents></ClassComonents>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/ClassComonents.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function ClassComonents() {
+  return (
+    <div>
+      <h1>Hello</h1> 
+      <p>world</p>
+    </div>
+  )
+}
